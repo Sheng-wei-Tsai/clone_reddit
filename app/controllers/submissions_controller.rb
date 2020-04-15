b0db323aefe6d78f227611a5247eb4c169610fbd
HenryTsai <henry88002605@gmail.com>
2020-04-15 11:12:53 +0800

    add submission-video in controller && video

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -62,6 +62,6 @@ class SubmissionsController < ApplicationController
 
 
     def submission_params
-      params.require(:submission).permit(:title, :body, :url, :submission_image )
+      params.require(:submission).permit(:title, :body, :url, :submission_image, :submission_video )
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_021618) do
+ActiveRecord::Schema.define(version: 2020_04_15_030232) do
 
   create_table "submissions", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_021618) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "submission_image"
+    t.string "submission_video"
   end
 
   create_table "users", force: :cascade do |t|
